5c0ed5f718bcae3e842d2e25a03b42c777cf0d08
olhamish <olgami1402@gmail.com>
2024-03-29 19:05:53 +0200

    add format argument

--- markdown2hml.py
+++ markdown2hml.py
@@ -1,18 +1,26 @@
 import sys
 import argparse
-from markdown_to_html.text_processing import process_text
+from markdown_convertor.text_processing import process_text
 
 
-def convert_markdown_to_html(input_file, output_file=None):
+def convert_markdown_to_format(input_file, output_file=None, output_format=None):
     try:
         with open(input_file, 'r') as f:
             markdown_text = f.read()
-            html_result = process_text(markdown_text)
+            if output_format is None:
+                output_format = 'ansi' if output_file is None else 'html'
+
+            processed_text = process_text(markdown_text, output_format)
+
             if output_file:
                 with open(output_file, 'w') as f_out:
-                    f_out.write(html_result)
+                    f_out.write(processed_text)
             else:
-                print(html_result)
+                if output_format == 'ansi':
+                    print(processed_text)
+                elif output_format == 'html':
+                    print(processed_text)
+
     except FileNotFoundError:
         print(f"Error: Input file '{input_file}' not found.", file=sys.stderr)
         sys.exit(1)
@@ -25,10 +33,11 @@ def main():
     parser = argparse.ArgumentParser(description="Convert Markdown to HTML.")
     parser.add_argument("input_file", help="Path to the input Markdown file")
     parser.add_argument("--output", "-o", help="Path to the output HTML file")
+    parser.add_argument("--format", "-f", choices=['ansi', 'html'],
+                        help="Output format (ansi/html). Default is ansi.")
 
     args = parser.parse_args()
-
-    convert_markdown_to_html(args.input_file, args.output)
+    convert_markdown_to_format(args.input_file, args.output, args.format)
 
 
 if __name__ == "__main__":
